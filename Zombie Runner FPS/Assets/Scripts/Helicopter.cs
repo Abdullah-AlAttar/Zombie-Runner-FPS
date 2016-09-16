9c74a9190cce0e56593a0078321646c6e5a4bda8
Abdullah Al Attar <abdullah.alattar@cis.asu.edu.eg>
2016-09-16 03:45:31 +0200

    reached 219 lesson
    added zombies and stuff

---
+++ Zombie Runner FPS/Assets/Scripts/Helicopter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Helicopter : MonoBehaviour {
+    public AudioClip callSound;
+    private bool called = false;
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("CallHeli") && ! called)
+        {
+            this.GetComponent<AudioSource>().clip = callSound;
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Zombie Runner FPS/Assets/Scripts/SunCycle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunCycle : MonoBehaviour {
+    [Tooltip("Number of minutes per second that pass, try 60")]
+    public float minutesPerSecond;
+    private Quaternion initialRotation;
+	void Start () {
+        initialRotation = this.transform.rotation;
+	}
+	
+
+	void Update () {
+        float angleThisFrame = Time.deltaTime / 360 * minutesPerSecond;
+        transform.RotateAround(transform.position,Vector3.forward,angleThisFrame);
+	}
+}
